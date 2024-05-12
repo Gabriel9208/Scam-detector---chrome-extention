0c8fb745163f687b9443fa795646e9f63eef4be2
Gabrieee <89693326+Gabriel9208@users.noreply.github.com>
2024-05-12 22:30:05 +0800

    fix xpath

--- scraper.py
+++ scraper.py
@@ -1,11 +1,26 @@
-import asyncio
 import re
+import multiprocess
+import dill
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import StaleElementReferenceException
 
-re_rules = {
+keys = {
+        'addr': ['市', '縣'], 
+        'tel': ['客服', '專線', '電話', '市話', '專線'],
+        'phone': ['手機'],
+        'fax': ['傳真', 'fax', 'Fax'],
+        'num': ['統編', '統一編號'],
+        'mail': ['信箱', 'mail', 'email', '郵件'],
+        'company': ['股份有限公司', '營業人', '經營者'],
+        #'other': ['意見反映', '關於我們', '聯絡我們'],
+    }
+
+def match_regex(key:str, webElement):
+    #print(f'Detacting {key}')
+    #print(webElement.text)
+    re_rules = {
         'addr': {'縣市': re.compile(r'(.*\n)?.{2}(市|縣).+(號|號之\d+)(\d+樓|\d+樓之\d+)?'),}, 
         'tel': {'0800': re.compile(r'\b080(0|9)(-| |&nbsp;)*\d{3}(-| |&nbsp;)*\d{3}\b'),
                 '2-4-4': re.compile(r'(\(0\d\)|0\d)(-| |&nbsp;)*\d{4}(-| |&nbsp;)*\d{4}'),
@@ -18,20 +33,8 @@ re_rules = {
         'num': {'num': re.compile(r'\d{8}'),},
         'mail': {'email': re.compile(r'[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}'),},
         'company': {'公司': re.compile(r'[^0-9 \n\ta-zA-Z><?!@#$%\^&*"\'\\~`]+股份有限公司')},
-}
-
-keys = {
-        'addr': ['市', '縣'], 
-        'tel': ['客服', '專線', '電話', '市話', '專線'],
-        'phone': ['手機'],
-        'fax': ['傳真', 'fax', 'Fax'],
-        'num': ['統編', '統一編號'],
-        'mail': ['信箱', 'mail', 'email', '郵件'],
-        'company': ['股份有限公司', '營業人', '經營者'],
+        #'other': {},
     }
-
-def match_regex(key:str, webElement):
-    print(f'Detacting {key}')
     
     match = None
     for _, regex in re_rules[key].items():
@@ -40,9 +43,9 @@ def match_regex(key:str, webElement):
             match = match_obj.group(0)
             return match
                 
-    following_siblings =  webElement.find_elements(By.XPATH, "following-sibling::*")
+    following_siblings = webElement.find_elements(By.XPATH, "following-sibling::*")
     for element in following_siblings[:3]:
-        for regex in re_rules['tel']:
+        for _, regex in re_rules['tel'].items():
             match_obj = regex.search(element.text)            
             if match_obj:
                 match = match_obj.group(0)
@@ -75,18 +78,17 @@ def extract_info(category: str, key: str, webElement):
         case 'company': 
             if key in ['股份有限公司', '營業人', '經營者']:
                 result = match_regex('company', webElement)
+        #case 'other': 
+        #    if key in ['意見反映', '關於我們', '聯絡我們']:
+        #        result = match_regex('other', webElement)
         
     return result
-
-async def scrape_footer_elements(footer_elements):    
-    #find all elements under the footer_elementss of footer
-    print("scrape_footer_elements1")
-    inside_elements = footer_elements.find_elements(By.XPATH, r'//*')
-    print("scrape_footer_elements2")
+    
+def iterate_webelement(webElement):
     info = {}
-    if inside_elements:
+    if webElement:
         for category, keywords in keys.items():
-            for elmt in inside_elements:
+            for elmt in webElement:
                 if elmt.tag_name not in ['a', 'img', 'script']:
                     find = False
                     for key in keywords:
@@ -97,51 +99,76 @@ async def scrape_footer_elements(footer_elements):
                     if find: break
     else:
         print("Footer is empty.")
-    return info
+        return None
     
+    return info
 
-async def async_scraper(url:str):
+def scraper(url:str):
     firefoxOpt = Options()
     firefoxOpt.add_argument("-headless")
     '''options=firefoxOpt'''
-    firefoxOpt.add_argument("user-agent=haha")
+    #firefoxOpt.add_argument("user-agent=haha")
     driver = webdriver.Firefox(options=firefoxOpt)
     
     if not re.match(r'https://|http://', url):
-        print('Scraper cannot handle url format.')
-        exit(1)
+        raise('Scraper cannot handle url format')
+    
+    info = {}
     
     try:
         driver.get(url)
-
-        # list of footers and elements with 'footer' substring inside their class attribute         
-        footer_num = len(driver.find_elements(By.TAG_NAME, 'footer')) + len(driver.find_elements(By.XPATH, "//*[contains(@class, 'footer')]"))
-        tasks = []
-        for index in range(footer_num):
-            # Prevent StaleElementReferenceException
-            footer_elements = driver.find_element(By.TAG_NAME, 'footer') if index == 0 else \
-                                            driver.find_elements(By.XPATH, "//*[contains(@class, 'footer')]")[index - 1]
-            task = asyncio.create_task(scrape_footer_elements(footer_elements))     
-            tasks.append(await task)
-                
-        results = await asyncio.gather(*tasks)
-        return results
-    
+        
+        # list of footers and elements with 'footer' substring inside their class attribute 
+        footers = driver.find_elements(By.XPATH, '//footer//*[text()]') +\
+            driver.find_elements(By.XPATH, "//*[contains(@class, 'footer')]//*[text()]")
+        
+        #footer_list = []
+        for footer_elements in footers:
+            if footer_elements:                
+                #find all elements under the footer_elementss of footer        
+                if isinstance(footer_elements, list):
+                    for category, keywords in keys.items():
+                        for elmt in footer_elements:
+                            find = False
+                            for key in keywords:
+                                if key in elmt.text:
+                                    find = True
+                                    retv = extract_info(category, key, elmt)
+                                    if retv:
+                                        info[category] = extract_info(category, key, elmt)
+                                    break
+                            if find: break
+                else:
+                    for category, keywords in keys.items():
+                        for key in keywords:
+                            if key in footer_elements.text:
+                                find = True
+                                retv = extract_info(category, key, footer_elements)
+                                if retv:
+                                    info[category] = extract_info(category, key, footer_elements)
+                                break
+            else:
+                print("Footer is empty.")
+                return None
+        '''       
+        with multiprocess.Pool(processes=50) as pool:
+            results = pool.map(func=iterate_webelement, iterable=(footer_list,))
+        print(results)
+              '''  
+        return info
     except ConnectionAbortedError as e:
         print("Connection error in scraper: ", e)
+        '''
     except StaleElementReferenceException as e:
         print("Stale element reference in scraper: ", e)
     except Exception as e:
         print("Unknown error in scraper: ", e)
+        '''
     finally:
         driver.quit()
 
-async def main():
-    matchList = await async_scraper("https://www.bnext.com.tw/")
+if __name__ == "__main__":
+    #matchList = scraper("https://www.bnext.com.tw/") # 47sec
+    matchList = scraper("https://www.momoshop.com.tw/main/Main.jsp") # 1min
+    
     print(matchList)
-    '''
-    matchList = scraper("https://www.bnext.com.tw/")
-    print(matchList)
-    '''
-
-asyncio.run(main())
