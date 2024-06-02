9a69eda7e5d84dfd48590524e8bcd75339e4db68
Gabrieee <89693326+Gabriel9208@users.noreply.github.com>
2024-06-02 21:09:34 +0800

    faster scraper!

--- scraper.py
+++ scraper.py
@@ -1,88 +1,38 @@
 import re
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import StaleElementReferenceException
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
-
-
-def match_regex(key:str, webElement, index:int, driver):
-    #print(f'Detacting {key}')
-    #print(webElement.text)
-    re_rules = {
-        'addr': {'縣市': re.compile(r'(.*\n)?.{2}(市|縣).+[0-9] ?(號|號之\d+) ?(\d+ ?樓|\d+ ?樓之\d+){0,1}'),}, 
+                                                                                        #新北市新店區寶中路119之1號10樓
+re_rules = {
+        'addr': {'縣市': re.compile(r'[^A-Za-z!@#$%^&*()_+1234567890=?/;:{}<>,.~`-]{2}(市|縣)[^A-Za-z!@#$%^&*()_+1234567890=?/;:{}<>,.~`-]+[0-9]{1,3} ?(號|號之\d+|之\d+號) ?(\d+ ?樓|\d+ ?樓之\d+){0,1}'),}, 
         'tel': {'0800': re.compile(r'\b080(0|9)(-| |&nbsp;)*\d{3}(-| |&nbsp;)*\d{3}\b'),
                 '2-4-4': re.compile(r'(\(0\d\)|0\d)(-| |&nbsp;)*\d{4}(-| |&nbsp;)*\d{4}'),
                 '2-3-4': re.compile(r'(\(0\d\)|0\d)(-| |&nbsp;)*\d{3}(-| |&nbsp;)*\d{4}'),
                 '3-2-4': re.compile(r'(\(0\d{2}\)|0\d{2})(-| |&nbsp;)*\d{2}(-| |&nbsp;)*\d{4}'),
-                '886': re.compile(r'\+?886(-| |&nbsp;)*\d(-| |&nbsp;)*\d{4}(-| |&nbsp;)*\d{4}')}, # 886-2-00000000
+                '886': re.compile(r'\+?886(-| |&nbsp;)*\d(-| |&nbsp;)*\d{4}(-| |&nbsp;)*\d{4}')},
         'phone': {'09': re.compile(r'(\(09\d{2}\)|09\d{2})(-| |&nbsp;)*\d{3}(-| |&nbsp;)*\d{3}'),
                   '0809': re.compile(r'0809(-| |&nbsp;)*\d{3}(-| |&nbsp;)*\d{3}')},
         'fax': {'fax': re.compile(r'(\(0\d\)|0\d)(-| |&nbsp;)*\d{4}(-| |&nbsp;)*\d{4}')},
         'num': {'num': re.compile(r'\d{8}'),},
-        'mail': {'email': re.compile(r'[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}'),},
-        'company': {'公司': re.compile(r'[^0-9 \n\ta-zA-Z><?!@#$%\^&*"\'\\~`]+股份有限公司')},
+        'mail': {'email': re.compile(r'[A-Za-z][\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}'),},
         'Food Business Registration No.': {'food': re.compile(r'[A-Z]-\d{9,9}-\d{5,5}-\d')}
-        #'other': {},
     }
-    
-    match = None
-    try:
-        for _, regex in re_rules[key].items():
-            match_obj = regex.search(webElement.text)
-            if match_obj:
-                match = match_obj.group(0)
-                return match
-                    
-        following_siblings = webElement.find_elements(By.XPATH, "following-sibling::*")
-        for element in following_siblings[:3]:
-            for _, regex in re_rules['tel'].items():
-                match_obj = regex.search(element.text)            
-                if match_obj:
-                    match = match_obj.group(0)
-                    return match
-                
-    except StaleElementReferenceException as e:
-        we = driver.find_elements(By.XPATH, '//footer//*[text()]') +\
-            driver.find_elements(By.XPATH, "//*[contains(@class, 'footer')]//*[text()]")
-        we = we[index]
-        return match_regex(key, we, index, driver)
+         
             
-    return match
-            
-            
-def extract_info(category: str, key: str, webElement, index:int, driver):      
+def extract_info(category: str, html: list[str]):     
+    if category == 'num':
+        pass
+             
     result = None
-    match category:
-        case 'addr': 
-            if key == '市' or key == '縣':
-                result = match_regex('addr', webElement, index, driver)
-        case 'tel': 
-            if key in ['客服', '專線', '電話', '市話', '專線']:
-                result = match_regex('tel', webElement, index, driver)
-        case 'phone': 
-            if key == '手機':
-                result = match_regex('phone', webElement, index, driver)
-        case 'fax': 
-            if key in ['傳真', 'fax', 'Fax']:
-                result = match_regex('fax', webElement, index, driver)
-        case 'num': 
-            if key in ['統編', '統一編號']:
-                result = match_regex('num', webElement, index, driver)
-        case 'mail': 
-            if key in ['信箱', 'mail', 'email', '郵件']:
-                result = match_regex('mail', webElement, index, driver)
-        case 'company': 
-            if key in ['股份有限公司', '營業人', '經營者']:
-                result = match_regex('company', webElement, index, driver)
-        case 'Food Business Registration No.': 
-            if key == '食品業':
-                result = match_regex('Food Business Registration No.', webElement, index, driver)
-        #case 'other': 
-        #    if key in ['意見反映', '關於我們', '聯絡我們']:
-        #        result = match_regex('other', webElement, index, driver)
-        
+    for _, regex in re_rules[category].items():
+        for line in html:
+            match_obj = regex.search(line)
+            if match_obj:
+                result = match_obj.group(0)
+                return result                
+            
     return result
 
 keys = {
@@ -92,52 +42,94 @@ keys = {
             'fax': ['傳真', 'fax', 'Fax'],
             'num': ['統編', '統一編號'],
             'mail': ['信箱', 'mail', 'email', '郵件'],
-            'company': ['股份有限公司', '營業人', '經營者'],
             'Food Business Registration No.': ['食品業']
-            #'other': ['意見反映', '關於我們', '聯絡我們'],
         }
 
-def process_footer_element(footer_elements, index, driver):
+def process_footer_element(html: str, key_index):
     info = {}
-    if footer_elements:                
-        try:
-            #find all elements under the footer_elementss of footer        
-            if isinstance(footer_elements, list):
-                for category, keywords in keys.items():
-                    for elmt in footer_elements:
-                        print(elmt.text)
-                        find = False
-                        for key in keywords:
-                            if key in elmt.text:
-                                find = True
-                                retv = extract_info(category, key, elmt, index, driver)
-                                if retv:
-                                    info[category] = retv
-                                else:
-                                    info['null'] = 'NULL'
-                                break
-                        if find: break
-            else:
-                for category, keywords in keys.items():
-                    for key in keywords:
-                        if key in footer_elements.text:
-                            find = True
-                            retv = extract_info(category, key, footer_elements, index, driver)
-                            if retv:
-                                info[category] = retv
-                            else:
-                                info['null'] = 'NULL'
-                            break
-        except Exception as e:
-            print(e)
-            exit(1)
-    else:
-        print("Footer is empty.")
-        info['null'] = 'NULL'
+    category = None
+    key = None
+    match key_index:
+        case 0: 
+            key = keys['addr'][0]
+            category = 'addr'
+        case 1: 
+            key = keys['addr'][1]
+            category = 'addr'
+        case 2: 
+            key = keys['tel'][0]
+            category = 'tel'
+        case 3: 
+            key = keys['tel'][1]
+            category = 'tel'
+        case 4: 
+            key = keys['tel'][2]
+            category = 'tel'
+        case 5: 
+            key = keys['tel'][3]
+            category = 'tel'
+        case 6: 
+            key = keys['tel'][4]
+            category = 'tel'
+        case 7: 
+            key = keys['phone'][0]
+            category = 'phone'
+        case 8: 
+            key = keys['fax'][0]
+            category = 'fax'
+        case 9: 
+            key = keys['fax'][1]
+            category = 'fax'
+        case 10: 
+            key = keys['fax'][2]
+            category = 'fax'
+        case 11: 
+            key = keys['num'][0]
+            category = 'num'
+        case 12: 
+            key = keys['num'][1]
+            category = 'num'
+        case 13: 
+            key = keys['mail'][0]
+            category = 'mail'
+        case 14: 
+            key = keys['mail'][1]
+            category = 'mail'
+        case 15: 
+            key = keys['mail'][2]
+            category = 'mail'
+        case 16: 
+            key = keys['mail'][3]
+            category = 'mail'
+        case 17: 
+            key = keys['Food Business Registration No.'][0]
+            category = 'Food Business Registration No.'
+    
+    if key == "市":
+        pass 
+    
+    matches = re.finditer(key, html)
+    #lines = html.splitlines()
+
+    for match in matches:
+        match_start_index = match.start()
+        valid_content = html[match_start_index:]
+        lines = valid_content.splitlines()
+        
+        retv = None
+        if 4 > len(lines):
+            retv = extract_info(category, lines)
+        else:
+            retv = extract_info(category, lines[:4])
+            
+        if retv:
+            info[category] = retv
+        else:
+            info['null'] = 'NULL'
     
     return info
     
-def scraper(url:str, driver): 
+def scraper(url:str, driver: webdriver): 
     if not re.match(r'https://|http://', url):
         raise('Scraper cannot handle url format')
     
@@ -145,16 +137,17 @@ def scraper(url:str, driver):
     
     try:
         driver.get(url)
+        html = driver.page_source
         
-        # list of footers and elements with 'footer' substring inside their class attribute 
-        footers = driver.find_elements(By.XPATH, '//footer//*[text() and not(self::a)]') +\
-            driver.find_elements(By.XPATH, "//*[contains(@class, 'footer')]//*[text() and not(self::a)]")
-
-        #footer_list = []
-        
-        with ThreadPoolExecutor(max_workers=len(footers)) as executor:
-                futures = [executor.submit(process_footer_element, footer, index, driver) 
-                        for index, footer in enumerate(footers)]
+        start_index = html.find('footer')
+        if start_index != -1:
+            footer = html[start_index:]
+        else:
+            print("No footer found!")
+            exit(1)
+    
+        with ThreadPoolExecutor(max_workers=18) as executor:
+                futures = [executor.submit(process_footer_element, footer, index) for index in range (18)]
                 for future in as_completed(futures):
                     element_info = future.result()
                     info.update(element_info)
@@ -171,19 +164,20 @@ def scraper(url:str, driver):
         
    
 start = time.time()
-firefoxOpt = Options()
-firefoxOpt.add_argument("-headless")
-driver = webdriver.Firefox(options=firefoxOpt)
-    
+chromeOpt = webdriver.ChromeOptions()
+chromeOpt.add_argument("-headless")
+chromeOpt.add_argument('--blink-settings=imagesEnabled=false')
+driver = webdriver.Chrome(options=chromeOpt)
+
 matchList = None    
 try:
-    #matchList = scraper("https://www.momoshop.com.tw/main/Main.jsp", driver) # momo 1min -> 40sec
-    #matchList = scraper("https://www.gvm.com.tw/", driver) # 遠見雜誌 43sec -> 29sec
-    matchList = scraper("https://www.cht.com.tw/home/consumer", driver) # 中華電信 43sec -> 29sec
-    #matchList = scraper("https://www.bnext.com.tw/", driver) # 數位時代47sec -> 23sec
-    #matchList = scraper("https://www.nccc.com.tw/wps/wcm/connect/zh/home", driver) # 財團法人聯合信用卡處理中心全球資訊網 30sec -> 20sec
-    #matchList = scraper("https://www.104.com.tw/jobs/main/", driver) # 104 30sec -> 22sec
-    #matchList = scraper("https://www.591.com.tw/", driver) # 591 86sec -> 50sec
+    #matchList = scraper("https://www.momoshop.com.tw/main/Main.jsp", driver) # momo 1min -> 40sec -> 8
+    #matchList = scraper("https://www.gvm.com.tw/", driver) # 遠見雜誌 43sec -> 29sec -> 5
+    #matchList = scraper("https://www.cht.com.tw/home/consumer", driver) # 中華電信 43sec -> 29sec -> 8
+    #matchList = scraper("https://www.bnext.com.tw/", driver) # 數位時代47sec -> 23sec -> 6
+    #matchList = scraper("https://www.nccc.com.tw/wps/wcm/connect/zh/home", driver) # 財團法人聯合信用卡處理中心全球資訊網 30sec -> 20sec -> 4
+    #matchList = scraper("https://www.104.com.tw/jobs/main/", driver) # 104 30sec -> 22sec -> 4
+    #matchList = scraper("https://www.591.com.tw/", driver) # 591 86sec -> 50sec -> 4
     print(matchList)
     end = time.time()
     print("The time of execution of above program is :",
