3d6a75b14d81dec39ebc99d9957dc5593e9f23fd
葉衍巖 <89693326+Gabriel9208@users.noreply.github.com>
2024-09-01 14:52:22 +0800

    tls_check.py: test passed

--- TLS_check.py
+++ TLS_check.py
@@ -1,19 +1,32 @@
 import ssl, socket
-from whoisInfo import grab_domain
+from urllib.parse import urlparse
+def fetch_tls_cert(url: str) -> dict:
+    """
+    Fetches the TLS certificate for a given URL.
 
-def tls_cert(url: str):
-    domain = grab_domain(url)
+    Args:
+        url (str): The URL to fetch the TLS certificate for.
+
+    Returns:
+        dict: The TLS certificate as a dictionary.
+
+    Raises:
+        ssl.SSLError: If the certificate is invalid.
+        Exception: If there is an error connecting to the server.
+    """
+    domain = urlparse(url).netloc
     context = ssl.create_default_context()
     address = (domain, 443)
+
     try:
         with socket.create_connection(address) as sock:
             with context.wrap_socket(sock, server_hostname=address[0]) as wsock:
-                #print(wsock.getpeercert())
+                
                 return wsock.getpeercert() # dict
     except ssl.SSLError:
-        print("Invalid certificate.")
+        raise ssl.SSLError("Invalid certificate.")
     except Exception:
-        print("Connection error.")
+        raise Exception("Connection error.")
         
 # Just for testing
-#tls_cert("https://www.nvidia.com/zh-tw/")
+# fetch_tls_cert("https://www.nvidia.com/zh-tw/")
